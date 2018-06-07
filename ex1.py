76903ccd180f2136478f1399303e1d2f5f5f5289
ZinMarKyawww <ZinMarKyaw5550@gmail.com>
2018-06-07 14:30:01 +0630

    add ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,8 @@
 print("Hello World!")
 print("Hello Again.")
 print('Yay! Printing.')
+print("I like typing this.")
+print("This is fun.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
 
